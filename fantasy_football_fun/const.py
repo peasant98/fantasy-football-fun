ef64bdcf571a6605dde2a2b2fd685ef1e7f6e074
Matthew Strong <mast4878@colorado.edu>
2020-04-08 00:50:09 -0600

    fun with strings!

--- fantasy_football_fun/const.py
+++ fantasy_football_fun/const.py
@@ -8,3 +8,14 @@ POSITIONS = ["qb", "rb", "wr", "te", "e",
              "t", "g", "c", "ol", "dt", "de",
              "dl", "ilb", "olb", "lb", "cb", "s",
              "db", "k", "p"]
+
+# all of the terms users can order by
+ORDER_BY_TERMS = ["player", "year_id", "age",
+                  "height_in", "weight", "bmi",
+                  "g", "gs", "pass_cmp", "pass_att",
+                  "pass_inc", "pass_cmp_perc", "pass_yds",
+                  "pass_td", "pass_int", "pass_int_td",
+                  "pass_td_perc", "pass_int_perc", "pass_rating",
+                  "pass_sacked", "pass_sacked_yds", "pass_sacked_perc",
+                  "pass_yds_per_att", "pass_adj_yds_per_att"]
+
--- fantasy_football_fun/pfr/pfr_query.py
+++ fantasy_football_fun/pfr/pfr_query.py
@@ -13,20 +13,79 @@ class PRFQuery():
                  draft_year_min=1936, draft_year_max=2019,
                  draft_slot_min=1, draft_slot_max=500,
                  draft_pick_in_round="pick_overall",
-                 conference="any",
-                 draft_pos=["qb", "wr", "te", "rb"],
-                 c5val=1.0, order_by="pass_td", offset=0):
+                 is_hof=None,
+                 draft_positions=["qb", "wr", "te", "rb"],
+                 order_by="pass_td", offset=0, order_by_asc=False):
         self.base_string = "https://www.pro-football-reference.com/play-index/psl_finder.cgi?request=1&match=single"
-        assert year_end >= year_end, "year_end should be the same or higher as year_start."
-        if season_end != -1:
-            assert season_end >= season_start, "season_end should be the same or higher as season_start"
+        self.year_start, self.year_end = self.validate_range(year_start, year_end)
+        self.season_start, self.season_end = self.validate_range(season_start, season_end)
+
+        self.draft_year_min, self.draft_year_max = self.validate_range(draft_year_min, draft_year_max)
+
+        self.draft_slot_min, self.draft_slot_max = self.validate_range(draft_slot_min, draft_slot_max)
+
+        self.age_min, self.age_max = self.validate_range(age_min, age_max)
+
+        self.positions = self.validate_list(C.POSITIONS, positions)
+
+        self.draft_positions = self.validate_list(C.POSITIONS, positions)
+
+        # order_by is the most important term here
+        self.order_by = self.validate_list(C.ORDER_BY_TERMS, order_by)
+
         provided_wrong_positions = 0 == len(set(positions) - set(C.POSITIONS))
         if provided_wrong_positions:
             raise ValueError("Wrong positions supplied! Please refer to const.py.")
+        else:
+            self.positions = positions
+        self.is_hof_string = ""
+        if is_hof is not None:
+            self.is_hof_string = "Y" if is_hof else "N"
 
-    def validate_range(self, min, max):
-        pass
 
+    def construct_query_string(self):
+        """
+        Constructs the query string to be used for webscraping.
+        """
+        self.standard_string = f"""{self.base_string}&year_min=
+                                   {self.year_start}&year_max=
+                                   {self.year_end}&season_start=
+                                   {self.season_start}&season_end=
+                                   {self.season_end}&draft_year_min=
+                                   {self.draft_year_min}&draft_year_max=
+                                   {self.draft_year_max}&draft_slot_min=
+                                   {self.draft_slot_min}&draft_slot_max=
+                                   {self.draft_slot_max}&draft_pick_in_round=
+                                   pick_overall&conference=any&c5val=1.0%&order_by=
+                                   """
+        self.pos_string = "".join([f"&pos[]={pos}" for pos in self.positions])
+
+        self.draft_pos_string = "".join([f"&draft_pos[]={pos}" for pos in self.draft_positions])
+        self.query_string = f"{self.standard_string}{self.pos_string}{self.draft_pos_string}"
+        """
+
+
+            p&c5val=1.0&order_by=pass_adj_yds_per_att
+
+
+
+
+        """
+
+
+    def validate_list(self, const_list, user_list):
+        provided_wrong_vals = 0 == len(set(user_list) - set(const_list))
+        if provided_wrong_vals:
+            raise ValueError("Wrong vaues supplied! Please refre to const.py.")
+        else:
+            return user_list
+
+    def validate_range(self, min, max, param_name, minus_one_valid=False):
+        if not minus_one_valid:
+            # two positive numbers must be provided
+            assert max >= min, f"""{param_name} end  xshould be the same of higher
+                                as {param_name} s"""
+            return min, max
 
 # https://www.pro-football-reference.com/play-index/psl_finder.cgi?
 # request=1&match=single&year_min=2019&year_max=2019&
