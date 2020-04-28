63d01d7009e8db62f7ab89c67c1d65662a670c40
Matthew Strong <mast4878@colorado.edu>
2020-04-27 22:32:17 -0600

    all code working

---
+++ fantasy_football_fun/pfr/FFDataset.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
+
+class FantasyFootballDataset(Dataset):
+    """
+    dataset for fantasy football data.
+    """
+    def __init__(self, csv, normalize=True):
+        # get the list of directories
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # over 30k entries long of player seasons
+        df = pd.read_csv(csv)
+        # get player names
+        self.player_names = np.unique(df['Player'].values)
+        self.correct_positions = ['QB', 'RB', 'WR', 'TE', 'K']
+        correct_df = df[df['position'].isin(self.correct_positions)]
+        sorted_df = correct_df.sort_values(['Player', 'Year'])
+        self.df = sorted_df.dropna()
+        ppg = 'FantPtPerGame'
+        self.pos_mapping = {}
+
+        self.player_names = np.unique(self.df['Player'].values)
+
+        for idx, position in enumerate(self.correct_positions):
+            self.pos_mapping[position] = idx
+
+        if normalize:
+            cols_to_norm = [ppg]
+            self.df[cols_to_norm] = self.df[cols_to_norm].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+
+    def __len__(self):
+        return len(self.player_names)
+
+    def __getitem__(self, idx):
+        player_name = self.player_names[idx]
+        player_seasons_df = self.df.loc[self.df['Player'] == player_name]
+        res = []
+        nums = []
+        for index, row in player_seasons_df.iterrows():
+            one_hot = np.zeros(len(self.correct_positions))
+            position = row['position']
+            nums.append(row['FantPtPerGame'])
+            one_hot[self.pos_mapping[position]] = 1.0
+            res.append(row['FantPtPerGame'])
+        plt.plot(nums, c='green')
+        plt.title(f'{position}: {player_name}')
+        plt.show()
+        """
+        data needed for net:
+        position, name, games FantPtPerGame
+
+        """
+        return torch.Tensor(res).to(self.device), torch.Tensor(one_hot).to(self.device)
+
+
+data = FantasyFootballDataset('finalized_players.csv', False)
--- fantasy_football_fun/pfr/models/models.py
+++ fantasy_football_fun/pfr/models/models.py
@@ -9,124 +9,134 @@ from torch.utils.data import Dataset, DataLoader
 from pypianoroll import Multitrack, Track
 import pandas as pd
 
+from fantasy_football_fun.pfr import FFDataset
 
-class FantasyFootballDataset(Dataset):
-    def __init__(self, root_folder, csv, transpose=False, is_conv=False):
-        # get the list of directories
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        # over 30k entries long
-        self.data_frame = pd.read_csv(os.path.join(root_folder, csv))
-        self.tracks_dict = {}
-        self.is_conv = is_conv
-        self.beat_resolution = 4
-        dirs = [d for d in os.listdir(root_folder) 
-                    if os.path.isdir(os.path.join(root_folder, d))]
-
-        # get all files
-        self.midi_files = []
-        accum = []
-        for d in dirs: 
-            midi_names = os.listdir(os.path.join(root_folder, d))
-            for i in range(len(midi_names)):
-
-                midi_names[i] = os.path.join(root_folder, d, midi_names[i])
-                try:
-                    # we can transpose
-                    multitrack = Multitrack(midi_names[i], beat_resolution=self.beat_resolution)
-                    for j in range(-6, 6):
-                        # need to augment data, so we can transpose the chords
-                        self.midi_files.append((midi_names[i], j))
-
-                except Exception as e:
-                    print(e)
-                    print(f"Couldn't read invalid file {midi_names[i]}")
-
-    def __len__(self):
-        return len(self.midi_files)
-
-    def __getitem__(self, idx):
-        # get multitrack
-        multitrack = Multitrack(self.midi_files[idx][0], beat_resolution=self.beat_resolution)
-        multitrack.transpose(self.midi_files[idx][1])
-        # get piano roll of whole song
-        pianoroll = multitrack.get_merged_pianoroll()
-        one_hot_pianoroll = np.where(pianoroll!=0, 1, pianoroll)
-        # shape is (num_time_steps, 128)
-        # input is some music
-        # output is music after?
-
-        return torch.Tensor(one_hot_pianoroll.astype(np.float))
-
-
-# note to self: 
 # use embeddings
 # using dense layer is crucial here to predict
 # lstm layer is a feature layer, need dense layer for probability
 # distribution
 
+
 class FantasyFootballLSTM(nn.Module):
     """
-    
+    LSTM model for fantasy football data.
     """
-    def __init__(self, input_dim, hidden_dim):
+    def __init__(self, input_dim, hidden_dim, fc1_dim=64, categories=5):
         super(FantasyFootballLSTM, self).__init__()
         # 2 stacked lstm
         # many folks use softmax to predict token, not whole vector
         self.lstm = nn.LSTM(input_dim, hidden_dim, 2)
-        self.fc = nn.Linear(hidden_dim, 10)
+        self.fc1 = nn.Linear(hidden_dim, fc1_dim)
+        self.fc2 = nn.Linear(fc1_dim, 128)
+        self.embedding = nn.Embedding(categories, 3)
+        self.position_fc = nn.Linear(15, 1)
+        self.fc3 = nn.Linear(128, 1)
 
-    def forward(self, x):
-        x = self.lstm(x, hidden)
-        x = self.fc(x[0])
+    def forward(self, x, hidden, pos):
+        x1 = self.lstm(x, hidden)
+        x1 = self.fc1(x1[0])
+        x1 = self.fc2(x1)
+        x2 = self.embedding(pos.type(torch.LongTensor).cuda())
+        x2 = torch.flatten(x2)
+        x2 = self.position_fc(x2)
 
-        return x
-
-
-class FantasyFootballModel():
-    def __init__(self):
-        super().__init__()
-
-class MidiLSTM(nn.Module):
-    def __init__(self, input_dim, hidden_dim):
-        super(MidiLSTM, self).__init__()
-        # 2 stacked lstm
-        # many folks use softmax to predict token, not whole vector
-        self.lstm = nn.LSTM(input_dim, hidden_dim, 2)
-        self.fc = nn.Linear(hidden_dim, 128)
-
-    def forward(self, x, hidden):
-        x = self.lstm(x, hidden)
-        x = self.fc(x[0])
-
-        return x
-
-# embed to EMBEDDING_DIM dimension vectors
-# adding reduction="sum" will be make gradient updates more extreme
-loss_function = nn.BCEWithLogitsLoss()
-model = MidiLSTM(128, 256)
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-md = MidiDataset("bach")
-
-# input is (num of sequences, batch size, time step)
-model.cuda()
-for epoch in range(50):
-    print("Epoch", epoch)
-    vals = list(range(len(md)))
-    np.random.shuffle(vals) 
-    for i, val in enumerate(vals):
-        hidden = (torch.randn(2, 1, 256).cuda(), torch.randn(2, 1, 256).cuda())
-        # values in dataset
-        optimizer.zero_grad()
-        pianoroll = md[i][:-1]
-        target = md[i][1:]
+        x3 = torch.add(x1, x2)
+        return self.fc3(x3)
 
 
-        x = pianoroll.view(-1, 1, 128).cuda()
-        target = target.view(-1, 1, 128).cuda()
-        output = model(x, hidden)
-        loss = loss_function(output, target)
-        loss.backward(retain_graph=True)
-        if i % 10 == 0:
-            print("Loss:", loss.item())
-        optimizer.step()
+# class FantasyFootballModel():
+#     def __init__(self):
+#         super().__init__()
+
+# class MidiLSTM(nn.Module):
+#     def __init__(self, input_dim, hidden_dim):
+#         super(MidiLSTM, self).__init__()
+#         # 2 stacked lstm
+#         # many folks use softmax to predict token, not whole vector
+#         # self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+
+#         self.lstm = nn.LSTM(input_dim, hidden_dim, 2)
+#         self.fc = nn.Linear(hidden_dim, 128)
+
+#     def forward(self, x, hidden):
+#         x = self.lstm(x, hidden)
+#         x = self.fc(x[0])
+
+#         return x
+
+# # embed to EMBEDDING_DIM dimension vectors
+# # adding reduction="sum" will be make gradient updates more extreme
+# loss_function = nn.BCEWithLogitsLoss()
+# model = MidiLSTM(128, 256)
+# optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# md = MidiDataset("bach")
+
+# # input is (num of sequences, batch size, time step)
+# model.cuda()
+# for epoch in range(50):
+#     print("Epoch", epoch)
+#     vals = list(range(len(md)))
+#     np.random.shuffle(vals) 
+#     for i, val in enumerate(vals):
+#         hidden = (torch.randn(2, 1, 256).cuda(), torch.randn(2, 1, 256).cuda())
+#         # values in dataset
+#         optimizer.zero_grad()
+#         pianoroll = md[i][:-1]
+#         target = md[i][1:]
+
+
+#         x = pianoroll.view(-1, 1, 128).cuda()
+#         target = target.view(-1, 1, 128).cuda()
+#         output = model(x, hidden)
+#         loss = loss_function(output, target)
+#         loss.backward(retain_graph=True)
+#         if i % 10 == 0:
+#             print("Loss:", loss.item())
+#         optimizer.step()
+
+if __name__ == '__main__':
+    dataset = FFDataset.FantasyFootballDataset("../finalized_players.csv", normalize=True)
+    print(dataset[1])
+    # exit()
+
+    career, pos_vec = dataset[1]
+
+    model = FantasyFootballLSTM(1, 128)
+    model.cuda()
+    hidden = (torch.randn(2, 1, 128).cuda(), torch.randn(2, 1, 128).cuda())
+    mycareer = career.view(-1, 1, 1)
+    out = model(mycareer, hidden, pos_vec)
+
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    loss_function = nn.MSELoss()
+    for epoch in range(50):
+        if epoch % 10 == 0:
+            torch.save(model.state_dict(), f"thirdmodel_{epoch}")
+        losses = []
+        print(f"NEW EPOCH {epoch}")
+        vals = list(range(len(dataset)))
+        np.random.shuffle(vals)
+        for i, idx in enumerate(vals):
+            hidden = (torch.randn(2, 1, 128).cuda(),
+                      torch.randn(2, 1, 128).cuda())
+            optimizer.zero_grad()
+            inputs = dataset[idx][0][:-1]
+            target = dataset[idx][0][1:]
+            if len(inputs) == 0:
+                continue
+            inputs = inputs.view(-1, 1, 1)
+            target = target.view(-1, 1, 1)
+            pos_onehot = dataset[idx][1]
+            out = model(inputs, hidden, pos_onehot)
+            loss = loss_function(out, target)
+            losses.append(loss.item())
+            loss.backward()
+            if i % 100 == 0:
+                print("Loss:", loss.item())
+                print(out)
+            optimizer.step()
+        mean_loss = np.mean(losses)
+        print(f"Mean LOSS: {mean_loss}")
+    #     # amount sentences
+
